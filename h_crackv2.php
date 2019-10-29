af9d1e47adcee8debaec8785da146fac627c580d
Sora Cyber Team <39432557+soracyberteam@users.noreply.github.com>
2019-10-29 09:15:24 -0400

    Add files via upload

--- h_crackv2.php
+++ h_crackv2.php
@@ -1,15 +1,32 @@
 <?php
 /*
 		{ Advanced Hash Cracker v2 }
-	@author : FilthyRoot
+	@author : FilthyRoot | Muhammad Khidhir Ibrahim
 	@github : soracyberteam
-	@date   : 16 May 2019
+	@date   : 16 May 2019 | Updated 29 Oct 2019
 
 	Copyright (c) 2019 Sora Cyber Team
-	INDONESIAN PEOPLE
 
-	GANTI EMAIL DI LINE KE 67 DAN 73
 */
+
+$username = "youremail@gmail.com";// SMTP Credential.
+$password = "yourpassword";// SMTP Credential.
+
+/*
+Readme!!
+
+   Check : https://support.google.com/mail/?p=BadCredentials#cantsignin
+   Then :
+   	1. Turn off 2-Step Verification
+   	2. Enable less secure apps
+   	3. Enable display unlock captcha
+
+   	I think, u need to make new google account for this.
+*/
+$youremail = "mainemail@gmail.com"; // This email will receive the cracked result.
+
+//Code Start
+include 'classes/class.phpmailer.php';
 error_reporting(0);
 set_time_limit(0);
 #$charset="aiueobcdfghjklmnpqrstvwxyzAIUEO1234567890#@!";
@@ -52,27 +69,42 @@ function crunch($a,$b,$c){
 		check($c.$charset[$i]);
 	}
 }
+function mailer($content,$youremail){
+	global $username,$password;
+
+	$mail = new PHPMailer;
+	$mail->IsSMTP();
+	$mail->SMTPSecure = 'ssl';
+	$mail->Host 	 = 'smtp.gmail.com';
+	$mail->Port 	 = 465;
+	#$mail->SMTPDebug = 2; //Remove # comment to debug SMTP connection
+	$mail->SMTPAuth  = True;
+	$mail->Username  = "$username";
+	$mail->Password  = "$password";
+	$mail->SetFrom("root@jogjakartahackerlink.or.id","Advanced Hash Cracker");
+	$mail->Subject 	 = "Hash Cracked!";
+	$mail->AddAddress("$youremail","Hash Cracker Client");
+	$mail->MsgHTML("<b>Result : </b><br>$content<br><br><b>Advanced Hash Cracker By Sora Cyber Team</b>");
+	return $mail->Send();
+}
 function check($pwnd){
-	global $type,$auth;
+	global $type,$auth,$youremail;
 	if(hasher($pwnd,$auth,$type)=="TRUE"){
-		$fh=fopen("h_crack.txt",a);
-		fwrite($fh,"$auth => $pwnd | Type : $type\n");
+		echo "[*] Done.\n\n{\n\t$auth = $pwnd\n}\n\n";
+		$fh=fopen("h_crack.txt",w);
+		if(fwrite($fh,"$auth = $pwnd | Type : $type\n")){
+			echo "[*] Saved to h_crack.txt\n";
+		}else{
+			echo "[*] Failed to save.\n";
+		}
 		fclose($fh);
 		$ip1=$_SERVER['REMOTE_ADDR'];
 		$ip2=$_SERVER['SERVER_ADDR'];
 		$content=file_get_contents('h_crack.txt');
-		$From="Hash_Cracker";
-		$Subject="Cracked!";
-		$Message="Server : $zzz // $ip2<br><pre>$content</pre>";
-		$Emails="soracyberteam@gmail.com";
-		$Name="hash_cracker";
-		$headers	= "MIME-Version: 1.0\r\n";
-		$headers .=	"Content-type:text/html;charset=UTF-8\r\n";
-		$headers	.= "From: <".$From.">\r\n";
-		$headers	.= "Cc: ".$Name."\r\n";
-		$Emails	= explode("\r\n", "soracyberteam@gmail.com");
-		foreach($Emails as $email) {
-			mail($email,$Subject,$Message,$headers);
+		if(mailer($content,$youremail)){
+			echo "[*] Sent to $youremail\n";
+		}else{
+			echo "[*] Failed to send SMTP. Check at line 80\n";
 		}
 		exit();
 	}else{
@@ -80,6 +112,7 @@ function check($pwnd){
 	}
 }
 if($auth && $type && $min && $max && $charset){
+	echo "[*] Cracking $auth :: $type\n";
 	for($x=$min;$x<$max+1;$x++){
 		crunch("$x","0","");
 	}
@@ -90,7 +123,14 @@ Type  : -md5
         -bcrypt (CMS Sekolahku) (SLOWER)
         -sha1
 
-Example : php ".$_SERVER['PHP_SELF']." '594f803b380a41396ed63dca39503542' md5 5 8 'abcdefghijklmnopqrstuvwxyz' > /dev/null 2>&1
+Example : 
+
+Background Process : 
+php ".$_SERVER['PHP_SELF']." '594f803b380a41396ed63dca39503542' md5 5 8 'abcdefghijklmnopqrstuvwxyz' &>/dev/null &
+
+Foreground Process : 
+php ".$_SERVER['PHP_SELF']." '594f803b380a41396ed63dca39503542' md5 5 8 'abcdefghijklmnopqrstuvwxyz'
+
 Copyright (c) 2019 Sora Cyber Team 
 ";
 }
